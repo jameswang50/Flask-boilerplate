df01bea091ade5d930c4f6434a0dd08c83ccb771
Frogbubble <frogbubble711@gmail.com>
2021-08-03 16:33:52 +0000

    CRUD

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -28,9 +28,14 @@ def create_app(test_config=None):
   from . import db
   db.init_app(app)
 
-  # register auth blueprint
+  # auth blueprint
   from . import auth
   app.register_blueprint(auth.bp)
+
+  # blog blueprint
+  from . import blog
+  app.register_blueprint(blog.bp)
+  app.add_url_rule('/', endpoint='index')
 
   # a simple page that says hello
   @app.route('/hello')
